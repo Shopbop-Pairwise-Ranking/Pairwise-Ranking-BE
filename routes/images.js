932192e39290130c9c530f82b320ef88dd3e0325
ramvaidya05 <72759841+ramvaidya05@users.noreply.github.com>
2024-10-27 20:30:54 -0500

    backend apis commit
    create a backendApi folder and use the below folder structure
    package.json
    server.js
    /routes folder
    images.js
    /data folder
    rankings.json
    
    after this run npm install to install all dependencies

---
+++ routes/images.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/getImages', async (req, res) => {
+  try {
+    const { q, limit, minPrice, maxPrice, dept, sort, offset } = req.query;
+    const params = {
+      q: q || '',
+      limit: limit && !isNaN(limit) ? parseInt(limit) : 40,
+      minPrice: minPrice || '',
+      maxPrice: maxPrice || '',
+      dept: dept || 'WOMENS',
+      lang: 'en-US',
+      allowOutOfStockItems: 'false',
+      siteId: '1006',
+      sort: sort || '',
+      offset: offset && !isNaN(offset) ? parseInt(offset) : 0,
+    };
+
+    Object.keys(params).forEach(
+      (key) => (params[key] === '' || params[key] === undefined) && delete params[key]
+    );
+
+    const headers = {
+      accept: 'application/json',
+      'Client-Id': process.env.CLIENT_ID,
+      'Client-Version': process.env.CLIENT_VERSION,
+    };
+
+    const response = await axios.get('https://api.shopbop.com/public/search', {
+      params,
+      headers,
+    });
+
+    const products = response.data.products || [];
+    const baseImageUrl = 'https://m.media-amazon.com/images/G/01/Shopbop/p';
+    const imageUrls = [];
+
+    products.forEach((item) => {
+      const product = item.product;
+      if (product && product.colors) {
+        product.colors.forEach((color) => {
+          if (color.images) {
+            color.images.forEach((image) => {
+              const imageUrl = baseImageUrl + image.src;
+              imageUrls.push(imageUrl);
+            });
+          }
+        });
+      }
+    });
+
+    res.json({ imageUrls });
+  } catch (error) {
+    console.error('Error fetching images:', error.message);
+    res.status(500).json({ error: 'Error fetching images' });
+  }
+});
+
+module.exports = router;
